a28318c311a53770dbc8a47c5a8c524db85452c7
mrinmoy15th <mrinmoy15th@gmail.com>
2026-08-05 20:28:06 +0530

    Test Added

---
+++ JoonsWeb.Tests/JoonsWebUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using JoonsWeb.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoonsWeb.Tests;
+
+public class JoonsWebUnitTest
+{
+    [Fact]
+    public void Index_ReturnsAViewResult()
+        {
+            // 1. Arrange - Setup your controller instances and data
+            var controller = new HomeController();
+
+            // 2. Act - Execute the method you are testing
+            var result = controller.Index();
+
+            // 3. Assert - Verify the outcome matches expectations
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+        }
+}
